d8a78b0e0fed320474e914ba3a4787a3f723e4bb
RCD Hui <rcdhui@gmail.com>
2021-11-28 17:13:40 +0800

    findMaxPathSun

--- src/test/java/com/ricardohui/maximumPathSum/MaximumPathSumTest.java
+++ src/test/java/com/ricardohui/maximumPathSum/MaximumPathSumTest.java
@@ -6,18 +6,16 @@ import static org.junit.jupiter.api.Assertions.*;
 class MaximumPathSumTest {
 
     @Test
+    void canHandleNullNode(){
+        assertEquals(0, MaximumPathSum.findMaximumPathSum(null));
+    }
+
+    @Test
     void shouldEqual6() {
         TreeNode root = new TreeNode(1);
         root.left = new TreeNode(2);
         root.right = new TreeNode(3);
         assertEquals( 6, MaximumPathSum.findMaximumPathSum(root));
-        root.left.left = new TreeNode(1);
-        root.left.right = new TreeNode(3);
-        root.right.left = new TreeNode(5);
-        root.right.right = new TreeNode(6);
-        root.right.left.left = new TreeNode(7);
-        root.right.left.right = new TreeNode(8);
-        root.right.right.left = new TreeNode(9);
     }
 
     @Test
@@ -39,6 +37,21 @@ class MaximumPathSumTest {
     void shouldEqualNeg4(){
         TreeNode root = new TreeNode(-1);
         root.left = new TreeNode(-3);
-        assertEquals( -4, MaximumPathSum.findMaximumPathSum(root));
+        assertEquals( -1, MaximumPathSum.findMaximumPathSum(root));
+    }
+
+    @Test
+    void shouldEqualNeg4WithRightSubtree(){
+        TreeNode root = new TreeNode(-1);
+        root.left = new TreeNode(-3);
+        root.right   = new TreeNode(0);
+        assertEquals( 0, MaximumPathSum.findMaximumPathSum(root));
+    }
+
+
+    @Test
+    void handleNullRecursion(){
+
+        assertEquals(0, MaximumPathSum.recursion(null));
     }
 }
