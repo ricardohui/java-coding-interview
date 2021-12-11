b717b573b67211613d8bdd724a9ab7f42bd45311
RCD Hui <rcdhui@gmail.com>
2021-12-11 09:04:04 +0800

    init commit

---
+++ src/main/java/com/ricardohui/javaCodingInterview/subset/CountUniqueTrees.java
@@ -0,0 +1,26 @@
+package com.ricardohui.javaCodingInterview.subset;
+
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+};
+
+class CountUniqueTrees {
+    public int countTrees(int n) {
+        // TODO: Write your code here
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        CountUniqueTrees ct = new CountUniqueTrees();
+        int count = ct.countTrees(3);
+        System.out.print("Total trees: " + count);
+    }
+}
